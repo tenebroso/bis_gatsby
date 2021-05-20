a491c9b5a21f82a37b15492617c977e1fc2de1fc
Jon Bukiewicz <jon@bukiewi.cz>
2021-05-20 15:32:31 -0500

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,25 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-75446459-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 20,
+        // Defers execution of google analytics script after page load
+        defer: true,
+        cookieDomain: "boomerangis.com",
+      },
+    },
   ],
 };
